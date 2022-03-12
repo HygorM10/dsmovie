12f86ed513d1ef83dbb7c5026437168abec203c6
Hygor Martins <hygor.martins74@gmail.com>
2022-03-11 23:52:36 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/hygor/dsmovie/entities/Movie.java
@@ -0,0 +1,30 @@
+package com.hygor.dsmovie.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_movie")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Movie {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	private Double score;
+	private Integer count;
+	private String image;
+	
+}
